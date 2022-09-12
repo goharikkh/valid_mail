63802336586a1cb1154f7804abdf304d7ac847b7
goharikkh <100861779+goharikkh@users.noreply.github.com>
2022-09-12 10:04:24 +0400

    Add files via upload

---
+++ valid mail.py
@@ -0,0 +1,13 @@
+import re
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+def check(email):
+    if re.fullmatch(regex, email):
+        return True
+    return False
+
+
+email = "goharik.khs@gmail.com"
+email2 = "ghiwdiw.com"
+print(check(email))
+print(check(email2))
